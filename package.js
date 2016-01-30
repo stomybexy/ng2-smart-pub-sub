7f183bb772266b152fb8d9b1f640e8f0c6aac00f
stomybexy <jonatan.sombie@gmail.com>
2016-01-30 11:16:27 +0000

    Update package.js

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
     // Brief, one-line summary of the package.
     summary: 'Publish and subscribe to data sets without rewriting queries on client with angular2-meteor.',
     // URL to the Git repository containing the source code for this package.
-    git: 'git@github.com:stomybexy/ng2-smart-pub-sub.git',
+    git: 'https://github.com/stomybexy/ng2-smart-pub-sub.git',
     // By default, Meteor will default to using README.md for documentation.
     // To avoid submitting documentation, set this field to null.
     documentation: 'README.md'
