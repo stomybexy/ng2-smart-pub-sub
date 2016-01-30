dfa285a30936a2dd50ddab440ebb00466a102620
stomybexy <jonatan.sombie@gmail.com>
2016-01-30 11:23:33 +0000

    Update package.js

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'jonatan:ng2-smart-pub-sub',
-    version: '0.0.3',
+    version: '0.0.4',
     // Brief, one-line summary of the package.
     summary: 'Publish and subscribe to data sets without rewriting queries on client with angular2-meteor.',
     // URL to the Git repository containing the source code for this package.
